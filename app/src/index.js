4425f4f4d9490c605410f0b4f2025a63cb28bc08
Halefa <internet@halefa.com>
2020-02-24 17:01:50 +0100

    All the error messages, all the error messages, show me your fails

--- app/src/index.js
+++ app/src/index.js
@@ -5,50 +5,46 @@ import './index.css';
 const adjectives = ['Blue','Small','Soft','Expensive','Glittering'];
 const randAdjective = adjectives[Math.floor(Math.random() * adjectives.length)] + adjectives[Math.floor(Math.random() * adjectives.length)] + adjectives[Math.floor(Math.random() * adjectives.length)];
 
-class Toggle extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {isToggleOn: true};
-  
-      // This binding is necessary to make `this` work in the callback
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.setState(state => ({
-        isToggleOn: !state.isToggleOn
-      }));
-    }
-  
+class Toggle extends React.Component {  
     render() {
       return (
         <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
+          Generate!
         </button>
       );
     }
   }
 
 class UserName extends React.Component {
-constructor(props) {
-    super(props);
-    this.state= {adjective: randAdjective};
-}
-
     render () {
         return (
             <h1>Your name is {this.state.adjective}</h1>
         );
     }
 }
-
-  function App() {
-    return (
-      <div>
-        <UserName />
-        <Toggle />
-      </div>
-    );
+  class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {adjective: null};
+  
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      handleClick() {
+        this.setState(state => ({
+          adjective: state.randAdjective
+        }));
+      }
+      
+      render() {
+        return (
+            <div>
+              <UserName value={this.state.adjective} />
+              <Toggle />
+            </div>
+          );
+      }
   }
   
 
