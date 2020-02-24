166da0f45c420c64a8ca21993924deadd1fb5918
Halefa <internet@halefa.com>
2020-02-24 16:37:05 +0100

    Added an on/off button taken from somewhere

--- app/src/index.js
+++ app/src/index.js
@@ -2,19 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+const adjectives = ['Blue','Small','Soft','Expensive','Glittering'];
+const randAdjective = adjectives[Math.floor(Math.random() * adjectives.length)] + adjectives[Math.floor(Math.random() * adjectives.length)] + adjectives[Math.floor(Math.random() * adjectives.length)];
+
+class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+    }
+  }
+
+class UserName extends React.Component {
+constructor(props) {
+    super(props);
+    this.state= {adjective: randAdjective};
+}
+
+    render () {
+        return (
+            <h1>Your name is {this.state.adjective}</h1>
+        );
+    }
+}
+
+  function App() {
+    return (
+      <div>
+        <UserName />
+        <Toggle />
+      </div>
+    );
   }
   
-  const adjectives = ['Blue','Small','Soft','Expensive','Glittering'];
-  const name = adjectives[Math.floor(Math.random() * adjectives.length)];
-
-  const app = <Welcome name={name} />;
 
 // ======================
 
   ReactDOM.render(
-    app,
+    <App />,
     document.getElementById('root')
   );
 
