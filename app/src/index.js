109f86e19129c5d5e5dcd380bdee1efe6ded102a
Halefa <internet@halefa.com>
2020-02-25 13:33:10 +0100

    The basic basic basic bsic functionality of the basic version WORKSgit add . Fixed state issues, no components

--- app/src/index.js
+++ app/src/index.js
@@ -2,29 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const adjectives = ['Blue','Small','Soft','Expensive','Glittering'];
-let randAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-
   class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state= {adjective: null};
+        this.state= {
+          adjectives: ['Blue','Small','Soft','Expensive','Glittering'],
+          randAdjective: null,
+        };
   
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
       }
     
-      handleClick() {
-        
-        this.setState(state => ({          
-          adjective: state.randAdjective
-        }));
+      handleClick() {       
+        this.setState({          
+          randAdjective: this.state.adjectives[Math.floor(Math.random() * this.state.adjectives.length)] + this.state.adjectives[Math.floor(Math.random() * this.state.adjectives.length)] + this.state.adjectives[Math.floor(Math.random() * this.state.adjectives.length)],
+        });
       }
       
       render() {
         return (
             <div>
-              <h1>Your name is {this.state.adjective}</h1>
+              <h1>Your name is {this.state.randAdjective}</h1>
            
               <button onClick={this.handleClick}>
           Generate!
