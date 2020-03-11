1ed830325d606073c0d7456c3244da5794467a75
Halefa <internet@halefa.com>
2020-03-11 13:56:08 +0100

    BOOM - file saved, all ready to go!

--- scraper/index2.js
+++ scraper/index2.js
@@ -2,8 +2,10 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const chalk = require('chalk')
+const fs = require('fs')
 
 const url = 'https://twitchemotes.com/'
+const outputFile = 'Twitch-emotes.js'
 let TwitchEmotes = [];
 
 console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
@@ -17,13 +19,25 @@ const getWebsiteContent = async (url) => {
     $('body > div.container > div:nth-child(9) > .card-body > .row > .col-md-2').map((i, el) => {
       let emote = $(el).find('center').text();
       console.log(emote);
-      TwitchEmotes.push(emote);
+      TwitchEmotes.push("'" + emote + "'");
       console.log(TwitchEmotes);
+      exportResults(TwitchEmotes)
     })
 } catch (error) {
     console.error(error)
   }
 
 };
+
+const exportResults = (TwitchEmotes) => {
+    // write to a new file named 2pac.txt
+    fs.writeFile(outputFile, TwitchEmotes, (err) => {
+      // throws an error, you could also catch it here
+      if (err) throw err;
+    
+      // success case, the file was saved
+      console.log('Emotes saved!');
+    });
+    }
 
 getWebsiteContent(url)
