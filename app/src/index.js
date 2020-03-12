a4fba14457f201bc6fcaa5ddc5a657147b7e5383
Halefa <internet@halefa.com>
2020-03-12 16:41:54 +0100

    Aaaall the styles

--- app/src/index.js
+++ app/src/index.js
@@ -1,83 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {adjectives1} from './adjectives1';
-import {adjectives2} from './adjectives2';
-import {nouns} from './nouns';
-import {emotes} from './emotes';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { adjectives1 } from "./adjectives1";
+import { adjectives2 } from "./adjectives2";
+import { nouns } from "./nouns";
+import { emotes } from "./emotes";
+import "./index.css";
+import logo from "./logo.png";
 
-  class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {    
-          adjectives1: adjectives1,
-          adjectives2: adjectives2,
-          nouns: nouns,
-          emotes: emotes,
-          randName: null,
-          clicked: false,
-        };
-  
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-      }
-    
-      handleClick() {       
-        //capitalise_Words 
-        function capitalise(string)
-        {          
-        return string.charAt(0).toUpperCase() + string.slice(1);
-        }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logo: logo,
+      adjectives1: adjectives1,
+      adjectives2: adjectives2,
+      nouns: nouns,
+      randName: null,
+      clicked: false
+    };
 
-        // let us have a variable
-        let randAdj1 = this.state.adjectives1[Math.floor(Math.random() * this.state.adjectives1.length)];
-        let randAdj2 = this.state.adjectives2[Math.floor(Math.random() * this.state.adjectives2.length)];
-        let randNoun = this.state.nouns[Math.floor(Math.random() * this.state.nouns.length)];
-        let randEmote = this.state.emotes[Math.floor(Math.random() * this.state.emotes.length)];
-
-        this.setState({    
-          randName: capitalise(randAdj1) + capitalise(randAdj2) + capitalise(randNoun) + randEmote,
-          clicked: true,
-        })
-        
-        /*
-        this.setState({
-          randAdjective1: this.state.adjectives1[Math.floor(Math.random() * this.state.adjectives1.length)],
-          randName: this.state.randAdjective1.charAt(0).toUpperCase() + this.state.randAdjective1.slice(1),
-        });
- */
-      }
-      
-      render() {
-        if (this.state.clicked) {
-          return (
-            <div>
-              <h1>Your name is {this.state.randName}</h1>             
-           
-              <button onClick={this.handleClick}>
-          Again!
-        </button>
-            </div>
-          );
-        }
-        return (
-          <div>
-            <h1>Click the button to generate your new Twitch name!</h1>             
-         
-            <button onClick={this.handleClick}>
-        Generate!
-      </button>
-          </div>
-        );
-      }
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
   }
-  
+
+  handleClick() {
+    //capitalise_Words
+    function capitalise(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // let us have a variable
+    let randAdj1 = this.state.adjectives1[
+      Math.floor(Math.random() * this.state.adjectives1.length)
+    ];
+    let randAdj2 = this.state.adjectives2[
+      Math.floor(Math.random() * this.state.adjectives2.length)
+    ];
+    let randNoun = this.state.nouns[
+      Math.floor(Math.random() * this.state.nouns.length)
+    ];
+
+    this.setState({
+      randName:
+        capitalise(randAdj1) +
+        capitalise(randAdj2) +
+        capitalise(randNoun),
+      clicked: true
+    });
+  }
+
+  render() {
+    if (this.state.clicked) {
+      return (
+        <div className="container">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="headline-name">
+            <h1>Your name is </h1>
+
+          <span className="purple name">
+           {this.state.randName}
+          </span>
+          </div>
+
+          <div className="button">
+            <button className="" onClick={this.handleClick}>
+              Again!
+            </button>
+          </div>
+
+        <div className="footer">Made by <a href="https://twitch.tv/halefa">Halefa</a><br />
+        No affiliation with Twitch</div>
+      </div>
+      );
+    }
+    return (
+      <div className="container">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="headline-generate">
+          <h1>
+            Click the<br /> button to generate<br /> your new
+            <br />
+            <span className="purple"> Twitch name</span>
+          </h1>
+        </div>
+
+        <div className="button">
+          <button className="" onClick={this.handleClick}>
+            Generate!
+          </button>
+        </div>
+
+        <div className="footer">Made by <a href="https://twitch.tv/halefa">Halefa</a><br />
+        No affiliation with Twitch</div>
+      </div>
+    );
+  }
+}
 
 // ======================
 
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
